517589d010d17df0184324f387be3bd0d6a5b320
Timhamer <240159@edu.rocfriesepoort.nl>
2022-03-24 14:55:59 +0100

    Beöordelings pagina

--- test/Form2.Designer.cs
+++ test/Form2.Designer.cs
@@ -32,16 +32,28 @@ namespace test
             this.Gaverder = new System.Windows.Forms.Button();
             this.Gaterug = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.Opmerkingtoevoegen = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.ster2 = new System.Windows.Forms.PictureBox();
+            this.ster4 = new System.Windows.Forms.PictureBox();
+            this.ster3 = new System.Windows.Forms.PictureBox();
+            this.ster5 = new System.Windows.Forms.PictureBox();
+            this.ster1 = new System.Windows.Forms.PictureBox();
+            this.Opmerking = new System.Windows.Forms.TextBox();
             this.Leesmeer = new System.Windows.Forms.Button();
+            this.Puntinfo = new System.Windows.Forms.Label();
+            this.Beöordelingspunt = new System.Windows.Forms.Label();
+            this.Opmerkingtoevoegen = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.Sluiten = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ster2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -69,15 +81,127 @@ namespace test
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Gray;
-            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.Opmerking);
             this.panel2.Controls.Add(this.Leesmeer);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.Puntinfo);
+            this.panel2.Controls.Add(this.Beöordelingspunt);
             this.panel2.Controls.Add(this.Opmerkingtoevoegen);
             this.panel2.Location = new System.Drawing.Point(100, 119);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(400, 200);
             this.panel2.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.ster2);
+            this.panel4.Controls.Add(this.ster4);
+            this.panel4.Controls.Add(this.ster3);
+            this.panel4.Controls.Add(this.ster5);
+            this.panel4.Controls.Add(this.ster1);
+            this.panel4.Location = new System.Drawing.Point(25, 125);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 25);
+            this.panel4.TabIndex = 4;
+            // 
+            // ster2
+            // 
+            this.ster2.Image = global::test.Properties.Resources.Leegster;
+            this.ster2.Location = new System.Drawing.Point(32, 0);
+            this.ster2.Name = "ster2";
+            this.ster2.Size = new System.Drawing.Size(25, 25);
+            this.ster2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ster2.TabIndex = 7;
+            this.ster2.TabStop = false;
+            this.ster2.Click += new System.EventHandler(this.ster2_Click);
+            // 
+            // ster4
+            // 
+            this.ster4.Image = global::test.Properties.Resources.Leegster;
+            this.ster4.Location = new System.Drawing.Point(94, 0);
+            this.ster4.Name = "ster4";
+            this.ster4.Size = new System.Drawing.Size(25, 25);
+            this.ster4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ster4.TabIndex = 6;
+            this.ster4.TabStop = false;
+            this.ster4.Click += new System.EventHandler(this.ster4_Click);
+            // 
+            // ster3
+            // 
+            this.ster3.Image = global::test.Properties.Resources.Leegster;
+            this.ster3.Location = new System.Drawing.Point(63, 0);
+            this.ster3.Name = "ster3";
+            this.ster3.Size = new System.Drawing.Size(25, 25);
+            this.ster3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ster3.TabIndex = 2;
+            this.ster3.TabStop = false;
+            this.ster3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // ster5
+            // 
+            this.ster5.Image = global::test.Properties.Resources.Volster;
+            this.ster5.Location = new System.Drawing.Point(125, 0);
+            this.ster5.Name = "ster5";
+            this.ster5.Size = new System.Drawing.Size(25, 25);
+            this.ster5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ster5.TabIndex = 1;
+            this.ster5.TabStop = false;
+            this.ster5.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.ster5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // ster1
+            // 
+            this.ster1.Image = global::test.Properties.Resources.Leegster;
+            this.ster1.Location = new System.Drawing.Point(0, 0);
+            this.ster1.Name = "ster1";
+            this.ster1.Size = new System.Drawing.Size(25, 25);
+            this.ster1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ster1.TabIndex = 0;
+            this.ster1.TabStop = false;
+            this.ster1.Click += new System.EventHandler(this.ster1_Click);
+            // 
+            // Opmerking
+            // 
+            this.Opmerking.Location = new System.Drawing.Point(225, 75);
+            this.Opmerking.Multiline = true;
+            this.Opmerking.Name = "Opmerking";
+            this.Opmerking.Size = new System.Drawing.Size(150, 100);
+            this.Opmerking.TabIndex = 5;
+            this.Opmerking.Visible = false;
+            // 
+            // Leesmeer
+            // 
+            this.Leesmeer.BackColor = System.Drawing.Color.White;
+            this.Leesmeer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Leesmeer.Location = new System.Drawing.Point(99, 66);
+            this.Leesmeer.Name = "Leesmeer";
+            this.Leesmeer.Size = new System.Drawing.Size(75, 23);
+            this.Leesmeer.TabIndex = 4;
+            this.Leesmeer.Text = "Lees meer";
+            this.Leesmeer.UseVisualStyleBackColor = false;
+            this.Leesmeer.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Puntinfo
+            // 
+            this.Puntinfo.AutoSize = true;
+            this.Puntinfo.BackColor = System.Drawing.Color.White;
+            this.Puntinfo.Location = new System.Drawing.Point(25, 40);
+            this.Puntinfo.MaximumSize = new System.Drawing.Size(150, 50);
+            this.Puntinfo.MinimumSize = new System.Drawing.Size(150, 50);
+            this.Puntinfo.Name = "Puntinfo";
+            this.Puntinfo.Size = new System.Drawing.Size(150, 50);
+            this.Puntinfo.TabIndex = 2;
+            this.Puntinfo.Text = "uhasd sad  adhsuk askhha kuhdas hkuhku da\r\n\r\n";
+            // 
+            // Beöordelingspunt
+            // 
+            this.Beöordelingspunt.AutoSize = true;
+            this.Beöordelingspunt.Location = new System.Drawing.Point(25, 15);
+            this.Beöordelingspunt.Name = "Beöordelingspunt";
+            this.Beöordelingspunt.Size = new System.Drawing.Size(54, 13);
+            this.Beöordelingspunt.TabIndex = 1;
+            this.Beöordelingspunt.Text = "Test tekst";
+            this.Beöordelingspunt.Click += new System.EventHandler(this.label1_Click);
             // 
             // Opmerkingtoevoegen
             // 
@@ -89,44 +213,12 @@ namespace test
             this.Opmerkingtoevoegen.UseVisualStyleBackColor = true;
             this.Opmerkingtoevoegen.Click += new System.EventHandler(this.button3_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Test tekst";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(25, 40);
-            this.label2.MaximumSize = new System.Drawing.Size(150, 50);
-            this.label2.MinimumSize = new System.Drawing.Size(150, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 50);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "uhasd sad  adhsuk askhha kuhdas hkuhku da\r\n\r\n";
-            // 
-            // Leesmeer
-            // 
-            this.Leesmeer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.Leesmeer.Location = new System.Drawing.Point(99, 66);
-            this.Leesmeer.Name = "Leesmeer";
-            this.Leesmeer.Size = new System.Drawing.Size(75, 23);
-            this.Leesmeer.TabIndex = 4;
-            this.Leesmeer.Text = "Lees meer";
-            this.Leesmeer.UseVisualStyleBackColor = true;
-            this.Leesmeer.Click += new System.EventHandler(this.button4_Click);
-            // 
             // panel1
             // 
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.Gaterug);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -139,7 +231,7 @@ namespace test
             this.panel3.BackColor = System.Drawing.Color.DimGray;
             this.panel3.Controls.Add(this.Sluiten);
             this.panel3.Controls.Add(this.textBox1);
-            this.panel3.Location = new System.Drawing.Point(152, 98);
+            this.panel3.Location = new System.Drawing.Point(529, 23);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(300, 250);
             this.panel3.TabIndex = 3;
@@ -168,14 +260,6 @@ namespace test
             this.textBox1.Text = "vhggfghjknbvnhg";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(225, 75);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 100);
-            this.textBox2.TabIndex = 5;
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -189,6 +273,12 @@ namespace test
             this.Load += new System.EventHandler(this.Form2_Load);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ster2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ster1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
@@ -201,13 +291,19 @@ namespace test
         private System.Windows.Forms.Button Gaterug;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button Leesmeer;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Puntinfo;
+        private System.Windows.Forms.Label Beöordelingspunt;
         private System.Windows.Forms.Button Opmerkingtoevoegen;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button Sluiten;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Opmerking;
+        private System.Windows.Forms.PictureBox ster1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox ster2;
+        private System.Windows.Forms.PictureBox ster4;
+        private System.Windows.Forms.PictureBox ster3;
+        private System.Windows.Forms.PictureBox ster5;
     }
 }
--- test/Form2.cs
+++ test/Form2.cs
@@ -15,13 +15,273 @@ namespace test
         public Form2()
         {
             InitializeComponent();
+            //Console.WriteLine(rating1 + " " + rating2 + " " + rating3 + " " + rating4 + " " + rating5 + " " + rating6 + " " + rating6);
         }
 
         int pagina = 1;
+        
+        String beöordelingspunt1 = "Klantgericht";
+        String beöordelingspunt2 = "Proces";
+        String beöordelingspunt3 = "Planning";
+        String beöordelingspunt4 = "Standup";
+        String beöordelingspunt5 = "Beroepscompetentie";
+        String beöordelingspunt6 = "Samenwerken";
+        String beöordelingspunt7 = "Beroepshouding";
+
+        String Beöorderlingtext1 = "Hoe goed heb je op de klant gericht?";
+        String Beöorderlingtext2 = "Hoe verliep het proces?";
+        String Beöorderlingtext3 = "Hoe verliep de planning?";
+        String Beöorderlingtext4 = "Hoe verliepen de standups?";
+        String Beöorderlingtext5 = "Beroepscompetentie";
+        String Beöorderlingtext6 = "Hoe is het samenwerken gegaan?";
+        String Beöorderlingtext7 = "Beroepshouding";
+
+        String Beöorderlinginfo1 = "Hoe goed was je afgelopen periode op de klant gericht? Hier beoordeel je hoe goed je samen hebt gewerkt met de klant. Heb je alles gedaan waar naar gevraagd werd? Heb je goed overlegt met de klant over wat er wordt verwacht?";
+        String Beöorderlinginfo2 = "Hoe verliep het proces van de afgelopen periode? Beoordeel hier hoe jij vond dat het werk verliep. Verliep alles goed of ben je nog tegen enkele problemen aangelopen, en zo ja hoe heb je deze opgelost?";
+        String Beöorderlinginfo3 = "Hoe verliep de planning van de afgelopen periode? Ben je op schema gebleven met alle taken of ben je ergens op blijven haken? Of misschien loop je zelfs voor op schema. Het is misschien handig om bij de opmerking te laten weten waar het goed of fout ging.";
+        String Beöorderlinginfo4 = "Is er elke dag tijd gemaakt voor een standup met het team? Is er elke dag van de afgelopen periode een standup met het team geweest om te bespreken wat er vorige keer is gedaan en waar iedereen mee bezig gaat. Ben je ook tevreden met hoe de standups verliepen?";
+        String Beöorderlinginfo5 = "Beroepscompetentie";
+        String Beöorderlinginfo6 = "Hoe verliep de samenwerking met het team in de afgelopen periode? Ben je tevreden met de communicatie tussen ieder teamlid en wist je altijd waar elk lid aan toe was? Wilden de leden je helpen wanneer je vast zat op een probleem of wanneer je een vraag had?";
+        String Beöorderlinginfo7 = "Beroepshouding";
+
+        String Opmerking1 = "";
+        String Opmerking2 = "";
+        String Opmerking3 = "";
+        String Opmerking4 = "";
+        String Opmerking5 = "";
+        String Opmerking6 = "";
+        String Opmerking7 = "";
+
+        int rating1;
+        int rating2;
+        int rating3;
+        int rating4;
+        int rating5;
+        int rating6;
+        int rating7;
+
+        int rating = 0;
+
+        
+        
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            Beöordelingspunt.Text = beöordelingspunt1;
+            Puntinfo.Text = Beöorderlingtext1;
+            Leesmeer.FlatStyle = FlatStyle.Flat;
+            Leesmeer.FlatAppearance.BorderSize = 0;
+            Leesmeer.BackColor = Color.White;
+        }
 
+        private void Sterrentonen(int ratings)
+        {
+            switch (ratings)
+            {
+                case 1:
+                    {
+                        Ster1();
+                        break;
+                    }
+                case 2:
+                    {
+                        Ster2();
+                        break;
+                    }
+                case 3:
+                    {
+                        Ster3();
+                        break;
+                    }
+                case 4:
+                    {
+                        Ster4();
+                        break;
+                    }
+                case 5:
+                    {
+                        Ster5();
+                        break;
+                    }
+            }
+        }
+        private void Paginaopslaan()
+        {
+            switch (pagina)
+            {
+                case 1:
+                    {
+                        
+                        
+                        Opmerking1 = Opmerking.Text;
+                        rating1 = rating;
+
+                        break;
+                    }
+                case 2:
+                    {
+                        
+                        
+                        Opmerking2 = Opmerking.Text;
+                        rating2 = rating;
+                        break;
+                    }
+                case 3:
+                    {
+                        
+                        
+                        Opmerking3 = Opmerking.Text;
+                        rating3 = rating;
+                        
+                        break;
+                    }
+                case 4:
+                    {
+                        
+                        
+                        Opmerking4 = Opmerking.Text;
+                        rating4 = rating;
+                        break;
+                    }
+                case 5:
+                    {
+                        
+                       
+                        Opmerking5 = Opmerking.Text;
+                        rating5 = rating;
+                        break;
+                    }
+                case 6:
+                    {
+                        
+                        
+                        Opmerking6 = Opmerking.Text;
+                        rating6 = rating;
+                        break;
+                    }
+                case 7:
+                    {
+                        
+                        
+                        Opmerking7 = Opmerking.Text;
+                        rating7 = rating;
+
+                        break;
+                    }
+                case 8:
+                    {
+                        break;
+                    }
+            }
+        }
+        private void Paginatonen()
+        {
+            switch (pagina)
+            {
+                case 1:
+                    {
+                        Opmerking.Text = Opmerking1;
+                        textBox1.Text = Beöorderlinginfo1;
+                        Beöordelingspunt.Text = beöordelingspunt1;
+                        Puntinfo.Text = Beöorderlingtext1;
+                        Sterrentonen(rating1);
+                        break;
+                    }
+                case 2:
+                    {
+                        Opmerking.Text = Opmerking2;
+                        textBox1.Text = Beöorderlinginfo2;
+                        Beöordelingspunt.Text = beöordelingspunt2;
+                        Puntinfo.Text = Beöorderlingtext2;
+                        Sterrentonen(rating2);
+                        break;
+                    }
+                case 3:
+                    {
+                        Opmerking.Text = Opmerking3;
+                        textBox1.Text = Beöorderlinginfo3;
+                        Beöordelingspunt.Text = beöordelingspunt3;
+                        Puntinfo.Text = Beöorderlingtext3;
+                        Sterrentonen(rating3);
+                        break;
+                    }
+                case 4:
+                    {
+                        Opmerking.Text = Opmerking4;
+                        textBox1.Text = Beöorderlinginfo4;
+                        Beöordelingspunt.Text = beöordelingspunt4;
+                        Puntinfo.Text = Beöorderlingtext4;
+                        Sterrentonen(rating4);
+                        break;
+                    }
+                case 5:
+                    {
+                        Opmerking.Text = Opmerking5;
+                        textBox1.Text = Beöorderlinginfo5;
+                        Beöordelingspunt.Text = beöordelingspunt5;
+                        Puntinfo.Text = Beöorderlingtext5;
+                        Sterrentonen(rating5);
+                        break;
+                    }
+                case 6:
+                    {
+                        Opmerking.Text = Opmerking6;
+                        textBox1.Text = Beöorderlinginfo6;
+                        Beöordelingspunt.Text = beöordelingspunt6;
+                        Puntinfo.Text = Beöorderlingtext6;
+                        Sterrentonen(rating6);
+                        break;
+                    }
+                case 7:
+                    {
+                        Opmerking.Text = Opmerking7;
+                        textBox1.Text = Beöorderlinginfo7;
+                        Beöordelingspunt.Text = beöordelingspunt7;
+                        Puntinfo.Text = Beöorderlingtext7;
+                        Sterrentonen(rating7);
+                        break;
+                    }
+            }
+        }
+        private void Ster1()
+        {
+            ster1.Image = test.Properties.Resources.Volster;
+            ster2.Image = test.Properties.Resources.Leegster;
+            ster3.Image = test.Properties.Resources.Leegster;
+            ster4.Image = test.Properties.Resources.Leegster;
+            ster5.Image = test.Properties.Resources.Leegster;
+        }
+        private void Ster2()
+        {
+            ster1.Image = test.Properties.Resources.Volster;
+            ster2.Image = test.Properties.Resources.Volster;
+            ster3.Image = test.Properties.Resources.Leegster;
+            ster4.Image = test.Properties.Resources.Leegster;
+            ster5.Image = test.Properties.Resources.Leegster;
+        }
+        private void Ster3()
+        {
+            ster1.Image = test.Properties.Resources.Volster;
+            ster2.Image = test.Properties.Resources.Volster;
+            ster3.Image = test.Properties.Resources.Volster;
+            ster4.Image = test.Properties.Resources.Leegster;
+            ster5.Image = test.Properties.Resources.Leegster;
+        }
+        private void Ster4()
+        {
+            ster1.Image = test.Properties.Resources.Volster;
+            ster2.Image = test.Properties.Resources.Volster;
+            ster3.Image = test.Properties.Resources.Volster;
+            ster4.Image = test.Properties.Resources.Volster;
+            ster5.Image = test.Properties.Resources.Leegster;
+        }
+        private void Ster5()
+        {
+            ster1.Image = test.Properties.Resources.Volster;
+            ster2.Image = test.Properties.Resources.Volster;
+            ster3.Image = test.Properties.Resources.Volster;
+            ster4.Image = test.Properties.Resources.Volster;
+            ster5.Image = test.Properties.Resources.Volster;
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -31,24 +291,29 @@ namespace test
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Paginaopslaan();
             pagina--;
+            Paginatonen();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            panel3.Location = new Point(150, 100);
             panel3.Visible = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             
+            Paginaopslaan();
             pagina++;
-            MessageBox.Show(pagina.ToString());
+            Paginatonen();
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox2.Visible = true;
+            Opmerking.Visible = true;
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -62,6 +327,45 @@ namespace test
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ster1_Click(object sender, EventArgs e)
+        {
+            rating = 1;
+            Ster1();
+        }
+        private void ster2_Click(object sender, EventArgs e)
+        {
+            rating = 2;
+            Ster2();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            rating = 3;
+            Ster3();
+        }
+        private void ster4_Click(object sender, EventArgs e)
+        {
+            rating = 4;
+            Ster4();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            rating = 5;
+            Ster5();
+        }
+
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
         {
 
         }
